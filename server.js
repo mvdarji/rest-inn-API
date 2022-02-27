8b525b173dbece9d913639698d2b56f4f58bf10f
Manali Darji <you@example.com>
2022-02-26 23:44:51 -0500

    changed port for local development

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+// deployed this repo on heroku:
+// heroku link: https://rest-inn-api-mvdarji.herokuapp.com/
+
 const path = require('path');
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, 'db.json'))
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 1001;
+const port = process.env.PORT || 1000;
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+    console.log(`API Server is Running on port: ${port}`);
+});
